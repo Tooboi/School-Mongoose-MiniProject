186f34debc13fb902e13b5de70472512af2d47c7
Tooboi <japica123@gmail.com>
2023-01-17 11:24:13 -0500

    aggregates complete

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,9 +3,13 @@ const { ObjectId } = require('mongoose').Types;
 const { Student, Course } = require('../models');
 
 // TODO: Create an aggregate function to get the number of students overall
-const headCount = async () =>
-  Student.aggregate()
-    // Your code here
+const headCount = async (studentId) =>
+  Student.aggregate([
+    {
+      $count: 'studentCount'
+    }
+  ])
+    
     .then((numberOfStudents) => numberOfStudents);
 
 // Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
@@ -13,14 +17,18 @@ const grade = async (studentId) =>
   Student.aggregate([
     // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
     {
-      // Your code here
+      $match: {_id: ObjectId(studentId)}
     },
     {
       $unwind: '$assignments',
     },
     // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
-      // Your code here
+      $group: {
+        _id: ObjectId(studentId),
+      avg_grade: {$avg: '$assignments.score'}
+      }
+      
     },
   ]);
 
